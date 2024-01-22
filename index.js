c79b829499bc8a18488a473d5a6abd69bb80512b
Digita1Panda <prodigykyo@gmail.com>
2024-01-22 04:03:13 +0000

    added the color shaders when submit is pushed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const timeDisplayEl = $("#currentDay");
+const currentTime = dayjs().hour();
 
 function displayTime() {
   let currentTime = dayjs().format("DD MMM YYYY [at] hh:mm:ss a");
@@ -7,3 +8,31 @@ function displayTime() {
 displayTime();
 
 setInterval(displayTime, 1000);
+
+$(".saveBtn").on("click", function () {
+  // Get the data-number attribute of the clicked button
+  const dataNumber = $(this).data("number");
+  console.log($(this).data("number"));
+
+  const textareaElement = $(this).closest(".time-block").find(".description");
+
+  localStorage.setItem(`textBox`, textareaElement.val());
+  // Compare the data-number with the current hour
+  if (dataNumber < currentTime) {
+    // If the data-number is less than the current hour, add a class for past
+    textareaElement.addClass("past");
+  } else if (dataNumber === currentTime) {
+    // If the data-number is equal to the current hour, add a class for present
+    textareaElement.addClass("present");
+  } else {
+    // If the data-number is greater than the current hour, add a class for future
+    textareaElement.addClass("future");
+  }
+
+  $(".description").on("input", function () {
+    // Remove all existing color classes when text is deleted
+    if ($(this).val().trim() === "") {
+      $(this).removeClass("past present future");
+    }
+  });
+});
