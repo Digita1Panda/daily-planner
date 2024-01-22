a24a9c7afe4665affb69eaed797823921e8a443a
Digita1Panda <prodigykyo@gmail.com>
2024-01-22 05:41:34 +0000

    added local storage and removal of the local storage if string is empty

--- index.js
+++ index.js
@@ -9,30 +9,40 @@ displayTime();
 
 setInterval(displayTime, 1000);
 
-$(".saveBtn").on("click", function () {
-  // Get the data-number attribute of the clicked button
-  const dataNumber = $(this).data("number");
-  console.log($(this).data("number"));
+function updateTimeBlocks() {
+  $(".time-block").each(function () {
+    let dataNumber = $(this).find(".description").data("number");
 
-  const textareaElement = $(this).closest(".time-block").find(".description");
-
-  localStorage.setItem(`textBox`, textareaElement.val());
-  // Compare the data-number with the current hour
-  if (dataNumber < currentTime) {
-    // If the data-number is less than the current hour, add a class for past
-    textareaElement.addClass("past");
-  } else if (dataNumber === currentTime) {
-    // If the data-number is equal to the current hour, add a class for present
-    textareaElement.addClass("present");
-  } else {
-    // If the data-number is greater than the current hour, add a class for future
-    textareaElement.addClass("future");
-  }
-
-  $(".description").on("input", function () {
-    // Remove all existing color classes when text is deleted
-    if ($(this).val().trim() === "") {
-      $(this).removeClass("past present future");
+    if (dataNumber < currentTime) {
+      $(this).find(".description").addClass("past");
+    } else if (dataNumber === currentTime) {
+      $(this).find(".description").addClass("present");
+    } else {
+      $(this).find(".description").addClass("future");
     }
   });
+}
+
+updateTimeBlocks();
+
+$(".saveBtn").on("click", function () {
+  let text = $(this).closest(".time-block").find(".description").val();
+
+  let hour = $(this).closest(".time-block").find(".description").data("number");
+
+  if (text.trim() === "") {
+    // If the text is empty, remove the entry from localStorage
+    localStorage.removeItem("event_" + hour);
+  } else {
+    localStorage.setItem("event_" + hour, text);
+  }
+});
+
+$(".time-block").each(function () {
+  let hour = $(this).find(".description").data("number");
+  let savedEvent = localStorage.getItem("event_" + hour);
+
+  if (savedEvent) {
+    $(this).find(".description").val(savedEvent);
+  }
 });
